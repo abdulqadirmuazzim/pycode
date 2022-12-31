58d09c01d51bf1f900d8b2768f34fd1d7655554d
abdul <abdulqadimuazzim@gmail.com>
2022-12-31 02:40:37 +0100

    initial Commit

---
+++ run.py
@@ -0,0 +1,25 @@
+# A PROGRAM TO HACK A PASSWORD
+
+from som import rand
+import subprocess as sub
+
+ano = '0HFICxyeJvOCN'
+
+num = 0
+
+file = []
+
+for it in ano:
+    print("Searching...")
+    while len(file) == num:
+        process = sub.run(['python', 'som.py'],
+                          capture_output=True, text=True)
+        com = [file.append(val) for val in process.stdout if val == it]
+        if com:
+            print(f"found {process.stdout}")
+            break
+    num += 1
+
+output = ''.join(file)
+
+print(f'your password is {output}')
---
+++ som.py
@@ -0,0 +1,8 @@
+import random as rd
+import string
+
+rand = "".join(rd.choice(string.ascii_letters + string.digits))
+# ano = '0HFICxyeJvOCNl4b2kTBc6MbGI5ZrNq9'
+# file = [1, 3, 4, 5]
+
+print(rand)
